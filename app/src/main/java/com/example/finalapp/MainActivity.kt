2a91700a8e424ae11b5fdf450ad460a48b920055
EvgeniyaMelenteva <9210793@gmail.com>
2021-11-15 16:43:50 +0300

    initial commit

---
+++ app/src/main/java/com/example/finalapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.finalapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val zodiacList: List<Zodiac> = listOf(
+            Zodiac("Овен", "21 марта – 20 апреля", "огонь"),
+            Zodiac("Телец", "21 апреля – 20 мая", "земля"),
+            Zodiac("Близнецы", "21 мая – 21 июня", "воздух"),
+            Zodiac("Рак", "22 июня – 22 июля", "вода"),
+            Zodiac("Лев", "23 июля – 23 августа", "огонь"),
+            Zodiac("Дева", "24 августа – 23 сентября", "земля"),
+            Zodiac("Весы", "24 сентября – 23 октября", "воздух"),
+            Zodiac("Скорпион", "24 октября – 22 ноября", "вода"),
+            Zodiac("Стрелец", "23 ноября – 21 декабря", "огонь"),
+            Zodiac("Козерог", "22 декабря – 20 января", "земля"),
+            Zodiac("Водолей", "21 января – 20 февраля", "воздух"),
+            Zodiac("Рыбы", "21 февраля – 20 марта", "вода")
+        )
+
+
+        val zodiacRecyclerView: RecyclerView = findViewById(R.id.zodiac_recycler_view)
+        zodiacRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        zodiacRecyclerView.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+        zodiacRecyclerView.adapter = ZodiacAdapter(zodiacList)
+    }
+}
---
+++ app/src/main/java/com/example/finalapp/Zodiac.kt
@@ -0,0 +1,7 @@
+package com.example.finalapp
+
+data class Zodiac(
+    val title:String,
+    val dates: String,
+    val element: String
+)
